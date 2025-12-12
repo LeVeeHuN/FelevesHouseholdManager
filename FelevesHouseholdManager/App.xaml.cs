411571306ab6f84338148ed540db871992be644c
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-12 10:30:32 +0100

    Add project files.

---
+++ FelevesHouseholdManager/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FelevesHouseholdManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
